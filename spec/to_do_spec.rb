e608508e226df6ba41badb8afc812d9cef8349fd
Kevin Chen & David Abel <student@epicodus.com>
2015-04-28 09:50:01 -0700

    Adds first two specs to return a description for Task and start with an empty array. Passes both.

---
+++ spec/to_do_spec.rb
@@ -0,0 +1,17 @@
+require('rspec')
+require('to_do')
+
+describe(Task) do
+  describe("#description") do
+    it("lest you give it a description") do
+      test_task = Task.new("scrub the zebra")
+      expect(test_task.description()).to(eq("scrub the zebra"))
+    end
+  end
+
+  describe(".all") do 
+    it ("is empty at first") do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+end
